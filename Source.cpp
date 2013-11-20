463ee18f492b58d76ac0639c5c4a8e11916cd342
Grady <gradyduncan@gmail.com>
2013-11-19 22:52:13 -0500

    Rewrite to get working properly
    Using a totally different method am able to fill and randomly generate
    the entire board. Went from 2d array to 1d array and will use a for loop
    to break output.
    
    Bug: It does not create 2 zeros instead it creates 1 zero and 1 eight
    when filling the array.

--- Source.cpp
+++ Source.cpp
@@ -1,40 +1,29 @@
 #include <iostream>
-#include <cstdlib>
 #include <iomanip>
-
+#include <cstdlib>
+#include <algorithm>
+ 
 using namespace std;
-
+ 
 int main () {
+	int board[16], number;
+	number = 0;
 
-	int board[4][4], t, s, x, r1;
-	s = 0;
-	x = 0;
-	r1 = rand() % 4;
-
-/*	for(int i = 1; i < 17; i++){
-	t = rand() % 9;
-	board[t][t] = s;
-	if(i % 2 == 0)
-		s++;
-	}*/
-	for(r1; r1 < 4;){
-		do{
-			cout << board[r1][x] << endl;
-			cout << "r1: " << r1 << " X: " << x << endl;
-			x++;
-			cout << "X: " << x << endl;
-		}while(x < 4);
-		x = 0;
-	}
-	for(int i = 0; i < 4; i++){
-		do{
-			cout << board[i][x] << endl;
-			cout << "I: " << i << " X: " << x << endl;
-			x++;
-			cout << "X: " << x << endl;
-		}while(x < 4);
-		x = 0;
+/** Fills board[] with number **/
+	for(int i = 0; i < 16; i++){
+		board[i] = number;
+		if(i % 2 == 0)
+			number++;
 	}
 
+/** Shuffles board[] 100% randomly **/
+	srand(time(0));
+	random_shuffle(&board[0], &board[16]);
+	
+/** Prints board[] for testing **/
+	for(int i=0; i<16; i++){
+		cout << "Board: " << board[i] << endl;
+	}
+	
 	return 0;
 }
