de07fb8f40d0b7949899cab8de0b2a6391c3dd49
Grady <gradyduncan@gmail.com>
2013-11-19 23:53:50 -0500

    Created gameBoard[]
    Creates and outputs the actual gameBoard[]. Need to style this so its
    easy to see the grid.
    
    Possible board[] fix but would like better fix.

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,7 @@
 using namespace std;
  
 int main () {
-	int board[16], number;
+	int board[16], gameBoard[16], number, x, y;
 	number = 0;
 
 /** Fills board[] with number **/
@@ -15,6 +15,10 @@ int main () {
 		if(i % 2 == 0)
 			number++;
 	}
+	
+/** Possible fix for the numbers... would prefer better method so we can expand up to all 52 cards in a deck
+	board[16] = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8};
+******/
 
 /** Shuffles board[] 100% randomly **/
 	srand(time(0));
@@ -22,8 +26,24 @@ int main () {
 	
 /** Prints board[] for testing **/
 	for(int i=0; i<16; i++){
-		cout << "Board: " << board[i] << endl;
+		cout << "Board: " << i << " : " << board[i] << endl;
 	}
 	
+/** Fills gameBoard[] with 0 **/
+	for(int i = 0; i < 16; i++)
+		gameBoard[i] = 0;
+	
+/** Prints game board and gets user input **/
+	cout << "Match Game" << endl;
+	for(int i = 0; i < 16; i++){
+		cout << gameBoard[i] << " ";
+		if(i == 3 || i == 7 || i == 11 || i = 15)
+			cout << endl;
+	}
+	cout << "Welcome! Please enter your X value" << endl;
+	cin >> x;
+	cout << "Please enter your Y value" << endl;
+	cin >> y;
+
 	return 0;
 }
