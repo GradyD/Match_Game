6b5431b8903302d911425586b580b4f79bcba56e
Grady <gradyduncan@gmail.com>
2013-11-25 15:54:06 -0500

    Code clean up via functions and added Y values
    Added Y values to gameBoard so user can quickly find the desired number
    
    Moved code into functions for reuse
    
    Testing difficulty levels, currently not working

--- Source.cpp
+++ Source.cpp
@@ -10,17 +10,23 @@ using namespace std;
 int gameOver(int gameBoard[]);
 int welcomeScreen(int &start);
 int userHelp(int &start);
+int check(int start, int difficulty);
+void print(int gameBoard[]);
+void logo();
  
 int main() {
 /** Sets color **/
 system("color 4e");
     
+/** Creats vars **/
 	int gameBoard[16], first, second, start, difficulty;
 	start = 0;
-/** Fills board[] with number **/
+	difficulty = 0;
+
+/** Fills board[] with numbers **/
 	int board[16] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8};
 
-/** Shuffles board[] 100% randomly **/
+/** Shuffles board[] randomly **/
 	srand(time(0));
 	random_shuffle(&board[0], &board[16]);
         
@@ -32,29 +38,29 @@ system("color 4e");
 	welcomeScreen(start);
 
 /** Loops while invalid numbers are entered **/
-	do {
-		if(start == 1)
-			userHelp(start);
-		else if(start == 2){
-			cout << "1.Easy (Unlimited guesses)" << endl;
-			cout << "2.Medium (25 guesses)" << endl;
-			cout << "3.Hard (10 guesses)" << endl;
-			cout << "Please enter the number of the difficulty desired!" << endl;
-			cin >> difficulty;
-		}else{
-			cout << start << endl;
-			welcomeScreen(start);
-		}
-	}while((start != 1) || (start != 2));	
-                
+	/* Remove this commit to test options 1 and 2, leave to test game 
+	check(start, difficulty);
+	*/
+
+/** Difficulty currently not working **/
+	/*switch(difficulty){
+		case(1):
+			cout << "Unlimted guesses" << endl;
+			break;
+		case(2):
+			cout << "30 guesses" << endl;
+			break;
+		case(3):
+			cout << "16 guesses" << endl;
+			break;
+		default:
+			cout << "Error 001" << endl;
+	}*/
+
 /** Prints game board and gets user input **/
-        do{
-        for(int i = 0; i < 16; i++){ //Move to function!
-                cout << gameBoard[i] << " ";
-                                if(i == 3 || i == 7 || i == 11 || i == 15)
-                                        cout << endl;
-        }
-		cout << "Welcome! Please enter your first guess" << endl;
+	do{
+		print(gameBoard);
+		cout << "\nWelcome! Please enter your first guess" << endl;
 		cin >> first;
 		first = first - 1;
         cout << "Please enter your second guess" << endl;
@@ -73,16 +79,16 @@ system("color 4e");
         return 0;
 }
 
-/** Function to check for zeros **/
+/** Function to check for zeros on gameBoard **/
 int gameOver(int gameBoard[]){
         for(int i = 0; i < 16; i++){
-                if(gameBoard[i] == 0)
-                        return false;
+			if(gameBoard[i] == 0)
+				return false;
         };
-	 
 }
 
-int welcomeScreen(int &start) {
+/** Logo for resuse **/
+void logo(){
 	cout << "___  ___      _       _         _____                        " << endl;
 	cout << "|  \\/  |     | |     | |       |  ___\\                       " << endl;
 	cout << "| .  . | __ _| |_ ___| |__     | |     ____ ___  ___   ___    " << endl;
@@ -90,6 +96,11 @@ int welcomeScreen(int &start) {
 	cout << "| |  | | (_| | || (__| | | |   | |_\\ \\ (_| | | | | | | \\__/   " << endl;
 	cout << "\\_|  |_/ \__,_|\\__\\___|_| |_|    \\____/\\__,_|_| |_| |_|\\___|   " << endl;
 	cout << endl;
+}
+
+/** Displays Welcome Screen **/
+int welcomeScreen(int &start) {
+	logo();
 	cout << "1.Help/User Manual   2.Game on!" << endl;
 	cout << "Please enter a number: ";
 	cin >> start;
@@ -97,6 +108,7 @@ int welcomeScreen(int &start) {
 	return start;
 }
 
+/** Displays User Guide **/
 int userHelp(int &start) {
  	cout <<"User help" <<endl;
 	cout << endl;
@@ -108,10 +120,41 @@ int userHelp(int &start) {
 	cout << "13 14 15 16" << endl;
 	cout << "If they don't match try again, if they do select another 2 places until you win the game." << endl;
 	cout << "Press enter to play the game.";
-	/*string s;
-	getline(cin, s);*/
-	system("pause");
+	//cin.ignore(); This works but causes issues with the current way our code works
+	system("pause"); //Temp work around until above method is working
 	system("cls");
 	start = 2;
 	return start;
  }
+
+/** Displays level difficulty **/
+int check(int start, int difficulty){
+	do {
+		if(start == 1)
+			userHelp(start);
+		else if(start == 2){
+			cout << "1.Easy (Unlimited guesses)" << endl;
+			cout << "2.Medium (25 guesses)" << endl;
+			cout << "3.Hard (10 guesses)" << endl;
+			cout << "Please enter the number of the difficulty desired!" << endl;
+			cin >> difficulty;
+		}else{
+			cout << start << endl;
+			welcomeScreen(start);
+		}
+	}while((start != 1) || (start != 2));	
+
+	return start, difficulty;
+};
+
+void print(int gameBoard[]){
+	logo();
+		for(int i = 0; i < 16; i++){
+			cout.width(3); //Creats spacing
+			if(i == 0 || i == 4 || i == 8 || i == 12 || i == 16) //Creats Y number row
+				cout << i + 1 << " ";
+			cout << gameBoard[i] << " ";
+			if(i == 3 || i == 7 || i == 11 || i == 15) // Creats grid from 1D array
+				cout << endl;
+		}
+};
